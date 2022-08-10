48c443ca5109e0a049c504006435c5e65ccbc39a
Ceyhun <contact.ceyhun@gmail.com>
2022-08-10 11:46:39 +0400

    Completed Admin model added

---
+++ blog-management-v1.0/blog-management-v1.0/DataBase/Models/Admin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blog_management_v1._0.DataBase.Models
+{
+    public class Admin : User
+    {
+        //normal admin elave elemek ucun.
+        public Admin(string name, string lastName, string email, string password)
+            : base(name, lastName, email, password)
+        {
+        }
+
+        //sildikden sonra admin elave etmek ucun
+        public Admin(string name, string lastName, string email, string password, int id)
+            : base(name, lastName, email, password, id)
+        {
+
+        }
+
+        //Update etmek ucun
+        public Admin(string name, string lastName)
+            : base(name, lastName)
+        {
+
+        }
+
+        public override string GetUserInfo()
+        {
+            return $"Istifadeci id : {Id} , Istifadeci adi : {Name} , soyadi : {LastName} , emaili : {Email} , qeydiyyat tarixi : {RegistrationDate}";
+
+        }
+    }
+}
