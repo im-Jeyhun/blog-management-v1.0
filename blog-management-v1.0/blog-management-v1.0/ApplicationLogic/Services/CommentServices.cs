2c7512abfb3cae036f2e8d4ae32544f90807e746
Ceyhun <contact.ceyhun@gmail.com>
2022-08-10 12:56:03 +0400

    Completed CommentServices added

---
+++ blog-management-v1.0/blog-management-v1.0/ApplicationLogic/Services/CommentServices.cs
@@ -0,0 +1,41 @@
+﻿using blog_management_v1._0.ApplicationLogic.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blog_management_v1._0.ApplicationLogic.Services
+{
+    public class CommentServices
+    {
+        public static string GetCommentContent()
+        {
+            bool isEceptionValid;
+            string comment = null;
+            do
+            {
+                try
+                {
+                    Console.Write("Insert title : ");
+                    comment = Console.ReadLine();
+                    if (comment == "null")
+                    {
+                        throw new Exception();
+                    }
+                    isEceptionValid = false;
+                }
+                catch (Exception)
+                {
+
+                    isEceptionValid = true;
+                    Console.WriteLine("Seflik var");
+                }
+
+            } while (isEceptionValid || !Validations.IsLengthCorrect(comment, 10, 35));
+
+
+            return comment;
+        }
+    }
+}
