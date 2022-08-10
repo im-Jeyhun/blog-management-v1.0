ae4a28f20097620fa5eadca98472f10efe11fb63
Ceyhun <contact.ceyhun@gmail.com>
2022-08-10 11:50:38 +0400

    Completed Inbox model added.

---
+++ blog-management-v1.0/blog-management-v1.0/DataBase/Models/Inbox.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blog_management_v1._0.DataBase.Models
+{
+    public class Inbox : Entity<int>
+    {
+        public string Notfication { get; set; }
+
+        public User User { get; set; }
+
+        public Inbox(string notfication, User user)
+        {
+            Notfication = notfication;
+            User = user;
+        }
+    }
+}
