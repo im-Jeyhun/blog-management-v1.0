ede2ac45d1f516213d6358f17d408fd5a5b4fb3b
Ceyhun <contact.ceyhun@gmail.com>
2022-08-10 11:41:51 +0400

    Initial structure

---
+++ blog-management-v1.0/blog-management-v1.0/UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace blog_management_v1._0.UI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
