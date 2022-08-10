4196ee1195b3f206e4dc8d714e5650cfa1e80a8e
Ceyhun <contact.ceyhun@gmail.com>
2022-08-10 11:47:58 +0400

    Completed Blog model added

---
+++ blog-management-v1.0/blog-management-v1.0/DataBase/Models/Blog.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blog_management_v1._0.DataBase.Models
+{
+    public class Blog : Entity<string>
+    {
+
+        public User Owner { get; set; }
+
+        public string Title { get; set; }
+
+        public BlogStatus BlogStatus { get; set; }
+
+        public static List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public string Content { get; set; }
+
+
+
+        public Blog(User owner, string title, string content, BlogStatus blogStatus, string id = null)
+        {
+            Owner = owner;
+            Title = title;
+            Content = content;
+            if (id != null)
+            {
+                Id = id;
+            }
+            else
+            {
+
+                Id = BlogRepository.RandomCode;
+            }
+            CreatedAt = DateTime.Now;
+            BlogStatus = blogStatus;
+
+        }
+
+        public string GetBlogFullInfo()
+        {
+            return "Blog id : " + Id + " " + "Blog owner :" + Owner.Name + Owner.LastName + " " + "Blog title :" + Title + " " + "Blog content :" + Content + " " + "Blog creating time :" + CreatedAt + " " + "Blog status : " + BlogStatus;
+        }
+
+        public string GetBlogInfo()
+        {
+            return $"[{CreatedAt}][{Id}][{Owner.Name}{Owner.LastName}] \n ==={Title}=== \n {Content}";
+        }
+    }
