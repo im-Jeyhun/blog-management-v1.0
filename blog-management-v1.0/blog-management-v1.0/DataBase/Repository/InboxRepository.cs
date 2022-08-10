b8c2eb059be792e985731808b994f0ebe1e5bb56
Ceyhun <contact.ceyhun@gmail.com>
2022-08-10 12:34:24 +0400

    Completed InboxRepoistory added

---
+++ blog-management-v1.0/blog-management-v1.0/DataBase/Repository/InboxRepository.cs
@@ -0,0 +1,14 @@
+﻿using blog_management_v1._0.DataBase.Models;
+using blog_management_v1._0.DataBase.Repository.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blog_management_v1._0.DataBase.Repository
+{
+    public class InboxRepository : Repository<Inbox, int>
+    {
+    }
+}
